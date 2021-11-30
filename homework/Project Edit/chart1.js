508e447594776a59bbeb167f48e16bd017c7f176
jahaansaini <saini.ja@northeastern.edu>
2021-11-30 18:54:35 -0500

    i

---
+++ homework/Project Edit/chart1.js
@@ -0,0 +1,16 @@
+d3.select("#chart2")
+  .on("mousemove", function() {
+
+    var tooltip = d3.select("#tooltip")
+      .style("display", "block")
+      .style("top", d3.event.pageY + 20 + "px")
+      .style("left", d3.event.pageX + 20 + "px");
+
+    tooltip.select("#title").html("Graph of traveling trends");
+    tooltip.select("#value").html("__ passengers travelled through __ in the year ___");
+
+  })
+  .on("mouseout", function() {
+    d3.select("#tooltip")
+      .style("display", "none");
+  });
